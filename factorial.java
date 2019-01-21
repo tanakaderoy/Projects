7282fcee538ae2713ec5681fe9e171c04218cd8d
Tanaka Mazivanhanga <42681052+tanakaderoy@users.noreply.github.com>
2019-01-21 02:37:22 -0500

    this is a commit

---
+++ factorial.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class factorial {
+    public static int fact(int n) {
+    	if(n == 0) {
+		return 1;
+    	}else {
+    		return n *fact(n-1);
+    	}
+    	
+        
+    }
+
+	public static void main(String[] args) {
+		Random generator = new Random(); 
+		int i = generator.nextInt(10) + 1;
+		System.out.println(fact(i));
+
+	}
+
+}
